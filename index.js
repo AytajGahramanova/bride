3e35c43414e5087a7087dca1b85a2f9e1163813f
Aytaj Gahramanova <132328862+AytajGahramanova@users.noreply.github.com>
2025-07-14 09:37:40 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// main-swiper
+var swiper = new Swiper(".main-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 3000,
+  // },
+});
+
+// products-swiper
+var swiper = new Swiper(".products-swiper", {
+  slidesPerView: 7,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 7,
+      spaceBetween: 40,
+    },
+  },
+});
